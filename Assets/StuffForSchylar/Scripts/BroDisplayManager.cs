8b94247b2383b36f0ace400eaf44c77d8856e767
SanzianaTudose <tudosesanziana@gmail.com>
2022-02-25 15:19:24 +0100

    Added stats display using images instead of text

--- Assets/StuffForSchylar/Scripts/BroDisplayManager.cs
+++ Assets/StuffForSchylar/Scripts/BroDisplayManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class BroDisplayManager : MonoBehaviour {
     [SerializeField] private List<BeefCake> beefCakes;
@@ -11,10 +12,13 @@ public class BroDisplayManager : MonoBehaviour {
     [Header("UI Elements")]
     [SerializeField] private TMP_Text nameText;
     [SerializeField] private TMP_Text levelText;
-    [SerializeField] private TMP_Text strengthText;
-    [SerializeField] private TMP_Text speedText;
-    [SerializeField] private TMP_Text staminaText;
     [SerializeField] private TMP_Text costText;
+    [SerializeField] private GameObject strength;
+    [SerializeField] private GameObject speed;
+    [SerializeField] private GameObject stamina;
+
+    [SerializeField] private Color statFilledCol;
+    [SerializeField] private Color statEmptyCol;
     
     private int curBro = 0;
 
@@ -46,11 +50,31 @@ public class BroDisplayManager : MonoBehaviour {
     private void UpdateUI(BeefCake beefCake) {
         nameText.text = beefCake.broName;
         levelText.text = "level: " + beefCake.level;
-
-        strengthText.text = "strength: " + beefCake.strength;
-        speedText.text = "speed: " + beefCake.speed;
-        staminaText.text = "stamina: " + beefCake.stamina;
-
         costText.text = "cost: " + beefCake.cost;
+
+        // Update stats
+        UpdateStat(strength, beefCake.strength);
+        UpdateStat(stamina, beefCake.stamina);
+        UpdateStat(speed, beefCake.speed);
+    }
+
+    // Displays stat level using the child images 
+    private void UpdateStat(GameObject statObject, int level) {
+        if (level < 0 || level > statObject.transform.childCount)
+            Debug.LogError("BroDisplayManager: Stat level is invalid.");
+
+        for (int i = 0; i < statObject.transform.childCount; i++) {
+            GameObject child = statObject.transform.GetChild(i).gameObject;
+
+            // TODO: This is not final. Will probably be changed to different sprites instead of colors.
+            Image childImg = child.GetComponent<Image>();
+            if (childImg == null)
+                Debug.Log("BroDisplayManager: Image component not found on stat children.");
+
+            if (i < level)
+                childImg.color = statFilledCol;
+            else
+                childImg.color = statEmptyCol;
+        }
     }
 }
