cbe1997176f902e42f3cfa3f5fcdfe53ac90b2ff
SanzianaTudose <tudosesanziana@gmail.com>
2022-03-14 16:59:26 +0100

    Display character model

--- Assets/Scripts/Inventory/HireUIManager.cs
+++ Assets/Scripts/Inventory/HireUIManager.cs
@@ -21,11 +21,14 @@ public class HireUIManager : MonoBehaviour
     [SerializeField] GameObject rightArrow;
     [SerializeField] GameObject leftArrow;
 
+    [SerializeField] GameObject charModel;
+
     public void DisplayCurrentItem(ShopBeefCake curItem) {
         hireButton.GetComponent<Button>().interactable = true;
         hireButton.GetComponentInChildren<TMP_Text>().text = "HIRE";
 
         SetTextUI(curItem);
+        SetCharacterModel(curItem);
     }
 
     public void OnClickHire() {
@@ -50,4 +53,34 @@ public class HireUIManager : MonoBehaviour
         strengthText.text = source.strength.ToString();
         speedText.text = source.speed.ToString();
     }
+
+    #region Character Model methods
+    private void SetCharacterModel(ShopBeefCake item) {
+        BeefCakeData source = item.source;
+
+        // Destroy any already existing children of {charModel} object
+        foreach (Transform child in charModel.transform) {
+            Destroy(child.gameObject);
+        }
+
+        InstantiateModel(source.prefab);
+    }
+
+    private void InstantiateModel(GameObject prefab) {
+        // Instantiate new character model as a child of {charModel}
+        GameObject model = Instantiate(prefab);
+        model.transform.parent = charModel.transform;
+        
+        // Set its local transform to display it properly 
+        model.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        model.transform.localRotation = Quaternion.identity;
+        model.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        // Change {model} layer to UI (including its children)
+        model.layer = LayerMask.NameToLayer("UI");
+        foreach (Transform trans in model.GetComponentsInChildren<Transform>()) {
+            trans.gameObject.layer = LayerMask.NameToLayer("UI");
+        }
+    }
+    #endregion
 }
