9134280c5ef70e9e1ba18711bd6ee26a6d22b13f
Vaneanea <40149868+Vaneanea@users.noreply.github.com>
2022-04-06 14:02:30 +0200

    testing a new saving system, far from stable

--- Assets/Scripts/CrewScripts/CrewInventory.cs
+++ Assets/Scripts/CrewScripts/CrewInventory.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,14 +10,61 @@ using UnityEngine;
 public class CrewInventory : MonoBehaviour {
     public static CrewInventory instance { get; private set; }
 
-    [SerializeField] public List<CrewBeefCake> crew;
     
+     public List<CrewBeefCake> crew;
+    
+     public string persisterName;
+
     [Header("Inventory Display Variables")]
      public GameObject slotPrefab;
      public GameObject inventoryObject;
 
+    //protected void OnEnable()
+    //{
+    //    crew = new List<CrewBeefCake>();
+    //    Object[] yourBeefcakes = Resources.LoadAll("Data/CrewBeefCakes", typeof(CrewBeefCake));
+
+    //    foreach (CrewBeefCake beefCake in yourBeefcakes)
+    //    {
+    //        //var SOpath = AssetDatabase.GUIDToAssetPath(beefCakeName);
+    //        //var beefCake = AssetDatabase.LoadAssetAtPath<CrewBeefCake>(SOpath);
+
+    //        crew.Add(beefCake);
+    //    }
+
+    //    for (int i = 0; i < crew.Count; i++)
+    //    {
+    //        if (File.Exists(Application.persistentDataPath + string.Format("/{0}_{1}.pso", persisterName, i)))
+    //        {
+    //            BinaryFormatter bf = new BinaryFormatter();
+    //            FileStream file = File.Open(Application.persistentDataPath + string.Format("/{0}_{1}.pso", persisterName, i), FileMode.Open);
+    //            JsonUtility.FromJsonOverwrite((string)bf.Deserialize(file), crew[i]);
+    //            file.Close();
+
+    //        }
+    //        else
+    //        {
+    //            //Do Nothing
+    //        }
+    //    }
+    //}
+
+    //protected void OnDisable()
+    //{
+    //    for (int i = 0; i < crew.Count; i++)
+    //    {
+    //        BinaryFormatter bf = new BinaryFormatter();
+    //        FileStream file = File.Create(Application.persistentDataPath + string.Format("/{0}_{1}.pso", persisterName, i));
+    //        var json = JsonUtility.ToJson(crew[i]);
+    //        bf.Serialize(file, json);
+    //        file.Close();
+    //    }
+
+    //}
+
+
     private void Awake() {
-        
+
         CreateSingletonInstance();
         crew = new List<CrewBeefCake>();
         GetCrew();
@@ -30,7 +79,7 @@ public class CrewInventory : MonoBehaviour {
     public void Add(ShopBeefCake data) {
         CrewBeefCake beefCake = CreateNewCrewBeefCake(data);
         crew.Add(beefCake);
-        SaveCrewBeefCake(beefCake);
+        //SaveCrewBeefCake(beefCake);
         AddInventorySlot(beefCake, inventoryObject, slotPrefab);
     }
 
@@ -55,15 +104,50 @@ public class CrewInventory : MonoBehaviour {
         else instance = this;
     }
 
-    private void GetCrew() 
-    {
-        string[] lookFor = new string[] { "Assets/Resources/Data/CrewBeefCakes" };
-        string[] yourBeefcakes = AssetDatabase.FindAssets("t:" + typeof(CrewBeefCake).Name, lookFor);
+    //private void GetCrew() 
+    //{
+       
+    //    ///TODO ADHUST THIS TO persistentDataPath
+    //    //string[] lookFor = new string[] { "Assets/Resources/Data/CrewBeefCakes" };
+    //    //string[] yourBeefcakes = AssetDatabase.FindAssets("t:" + typeof(CrewBeefCake).Name, lookFor);
+    //    Object[] yourBeefcakes = Resources.LoadAll("Data/CrewBeefCakes", typeof(CrewBeefCake));
 
-        foreach (string beefCakeName in yourBeefcakes)
+    //    foreach (CrewBeefCake beefCake in yourBeefcakes)
+    //    {
+    //        //var SOpath = AssetDatabase.GUIDToAssetPath(beefCakeName);
+    //        //var beefCake = AssetDatabase.LoadAssetAtPath<CrewBeefCake>(SOpath);
+
+    //        crew.Add(beefCake);
+    //    }
+    //}
+
+    private void GetCrew()
+    {
+        //load data
+
+        string path = Application.persistentDataPath + "/YourBeefCakes";
+        DirectoryInfo d = new DirectoryInfo(path);
+        foreach (var file in d.GetFiles())
         {
-            var SOpath = AssetDatabase.GUIDToAssetPath(beefCakeName);
-            var beefCake = AssetDatabase.LoadAssetAtPath<CrewBeefCake>(SOpath);
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(file.DirectoryName, FileMode.Open);
+            CrewBeefCake beefCake = formatter.Deserialize(stream) as CrewBeefCake;
+            stream.Close();
+            crew.Add(beefCake);
+        }
+
+
+
+        ///TODO ADHUST THIS TO persistentDataPath
+        //string[] lookFor = new string[] { "Assets/Resources/Data/CrewBeefCakes" };
+        //string[] yourBeefcakes = AssetDatabase.FindAssets("t:" + typeof(CrewBeefCake).Name, lookFor);
+        Object[] yourBeefcakes = Resources.LoadAll("Data/CrewBeefCakes", typeof(CrewBeefCake));
+
+        foreach (CrewBeefCake beefCake in yourBeefcakes)
+        {
+            //var SOpath = AssetDatabase.GUIDToAssetPath(beefCakeName);
+            //var beefCake = AssetDatabase.LoadAssetAtPath<CrewBeefCake>(SOpath);
+
             crew.Add(beefCake);
         }
     }
@@ -84,6 +168,20 @@ public class CrewInventory : MonoBehaviour {
 
         beefCake.displayName = data.source.displayName;
 
+        //save data
+        BinaryFormatter formatter = new BinaryFormatter();
+        if (!Directory.Exists(Path.Combine(Application.persistentDataPath, "YourBeefCakes")))
+        {
+            //if it doesn't, create it
+            Directory.CreateDirectory(Path.Combine(Application.persistentDataPath, "YourBeefCakes"));
+
+        }
+        string path = Application.persistentDataPath + "/YourBeefCakes/" + beefCake.displayName;
+        Debug.Log("Saving at: " + path);
+        FileStream stream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(stream, beefCake);
+        stream.Close();
+
         return beefCake;
     }
 
@@ -91,6 +189,8 @@ public class CrewInventory : MonoBehaviour {
     {
         string path = "Assets/Resources/Data/CrewBeefCakes/" + beefCake.displayName + ".asset";
         AssetDatabase.CreateAsset(beefCake, path);
+        
+       // Resources.
     }
 
     
