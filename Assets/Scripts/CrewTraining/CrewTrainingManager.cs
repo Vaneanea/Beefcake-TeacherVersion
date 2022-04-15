2ac72ea03fd9d1cc94fe2c99a16a4c8bc94073e4
SanzianaTudose <tudosesanziana@gmail.com>
2022-04-15 17:45:31 +0200

    Completed training mini-game

--- Assets/Scripts/CrewTraining/CrewTrainingManager.cs
+++ Assets/Scripts/CrewTraining/CrewTrainingManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CrewTrainingManager : MonoBehaviour {
     [SerializeField] private CrewBeefCake curBeefCake;
 
+    [SerializeField] private int trainingTime;
+
     private TrainingUIManager uiManager;
 
     private void Start() {
@@ -12,6 +14,7 @@ public class CrewTrainingManager : MonoBehaviour {
         SetCurBeefCake();
 
         uiManager.OnStart(curBeefCake);
+        StartCoroutine(EndTrainingAfterSeconds(trainingTime));
     }
 
     private void OnEnable() {
@@ -35,5 +38,12 @@ public class CrewTrainingManager : MonoBehaviour {
     private void SetCurBeefCake() {
         // TODO: Implement this! Pass information from other scenes.
         // Right now, {curBeefCake} is set manually through the Editor
+    }
+
+    private IEnumerator EndTrainingAfterSeconds(float seconds) {
+        yield return new WaitForSeconds(seconds);
+
+        Debug.Log("END TRAINING SESSION");
+        // TODO: To be implemented.
     }
 }
--- Assets/Scripts/CrewTraining/TrainButtonController.cs
+++ Assets/Scripts/CrewTraining/TrainButtonController.cs
@@ -11,10 +11,13 @@ public class TrainButtonController : MonoBehaviour {
     [Space(10)]
     [SerializeField] private int perfectAmount;
 
+    private int perfectChain;
+
     public delegate void Trained(int trainAmount);
     public static event Trained OnTrain;
 
     private void Start() {
+        perfectChain = 0;
         LeanTween.scale(gameObject, new Vector3(minScale, minScale, minScale), scaleTime).setLoopPingPong();
     }
 
@@ -22,8 +25,11 @@ public class TrainButtonController : MonoBehaviour {
         float score = gameObject.transform.localScale.x;
 
         if (score > perfectThreshold) {
+            perfectChain++;
+
             if (OnTrain != null)
-                OnTrain(perfectAmount);
-        }
+                OnTrain(perfectAmount * perfectChain);
+        } else 
+            perfectChain = 0;
     }
 }
