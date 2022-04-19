d284bdffbd0889e68a9240e52e198e809297ac90
SanzianaTudose <tudosesanziana@gmail.com>
2022-04-19 15:46:17 +0200

    Code clean-up 🧹

--- Assets/Scripts/WashLoop/BubbleSystemController.cs
+++ Assets/Scripts/WashLoop/BubbleSystemController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BubbleSystemController : MonoBehaviour {
-    [SerializeField] private float alphaDecreaseRate = 0.1f;
+    [SerializeField] private float alphaDecreaseRate;
     [SerializeField] private Color startColor;
 
     private ParticleSystem bubbleSystem;
@@ -12,28 +12,15 @@ public class BubbleSystemController : MonoBehaviour {
         bubbleSystem = GetComponent<ParticleSystem>();
     }
 
-    private void Update() {
-        if (!Input.GetMouseButtonDown(0)) return;
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100)) {
-            if (hit.transform.gameObject == this.gameObject) {
-                OnClick();
-            }
-        }
-    }
-
-    private void SetStartColor(Color newColor) {
-        ParticleSystem.MainModule main = bubbleSystem.main;
-        main.startColor = newColor;
-    }
-
-    private void OnClick() {
+    public void OnClick() {
         startColor = new Color(startColor.r, startColor.g, startColor.b, startColor.a - alphaDecreaseRate);
         SetStartColor(startColor);
 
-        Debug.Log(startColor);
+        if (startColor.a <= 0.0f)
+            Destroy(gameObject);
+    }
+    private void SetStartColor(Color newColor) {
+        ParticleSystem.MainModule main = bubbleSystem.main;
+        main.startColor = newColor;
     }
 }
