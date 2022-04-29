4e7969eea52ec82f90e0de1e56ac7c9b6e675978
SanzianaTudose <tudosesanziana@gmail.com>
2022-04-29 14:39:01 +0200

    Paint square on Car texture

--- Assets/Scripts/WashLoop/WashableCar.cs
+++ Assets/Scripts/WashLoop/WashableCar.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Handles displaying the dirty car texture
+// Handles displaying the dirty Car texture
 public class WashableCar : MonoBehaviour {
 
+    [Header("Dependencies")]
     [SerializeField] private Texture2D dirtMaskTextureBase;
     [SerializeField] private Material material;
 
+    [Header("Brush Fields")]
+    [SerializeField] private Texture2D dirtBrush;
+
     private Texture2D dirtMaskTexture;
+
+    private void Awake() {
+        MakeDirtMask();
+    }
 
     private void Update() {
         if (Input.GetMouseButtonDown(0))
@@ -16,14 +24,42 @@ public class WashableCar : MonoBehaviour {
         
     }
 
+    private void MakeDirtMask() {
+        dirtMaskTexture = new Texture2D(dirtMaskTextureBase.width, dirtMaskTextureBase.height);
+        dirtMaskTexture.SetPixels(dirtMaskTextureBase.GetPixels());
+        dirtMaskTexture.Apply();
+
+        material.SetTexture("_DirtMask", dirtMaskTexture);
+    }
+
     private void WashCar() {
         RaycastHit hit;
 
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+        int layerMask = LayerMask.GetMask("WashableCar");
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, layerMask)) {
             Vector2 textureCoord = hit.textureCoord;
 
-            //TODO: Implement painting on the dirt mask texture
-        }
+            int pixelX = (int) (textureCoord.x * dirtMaskTexture.width);
+            int pixelY = (int) (textureCoord.y * dirtMaskTexture.height);
 
+            Vector2Int paintPixelPosition = new Vector2Int(pixelX, pixelY);
+
+            // Paint square in Dirt Mask
+            int squareSize = 32;
+            int pixelXOffset = pixelX - (dirtBrush.width / 2);
+            int pixelYOffset = pixelY - (dirtBrush.height / 2);
+
+            for (int x = 0; x < squareSize; x++) {
+                for (int y = 0; y < squareSize; y++) {
+                    dirtMaskTexture.SetPixel(
+                        pixelXOffset + x,
+                        pixelYOffset + y,
+                        Color.black
+                    );
+                }
+            }
+
+            dirtMaskTexture.Apply();
+        }
     }
 }
