787d1dd83a9d75bf885210f971cff05ec1099ef2
SanzianaTudose <tudosesanziana@gmail.com>
2022-05-09 15:12:23 +0300

    Clean-up pop-up behavior
    + moved logic from Car.cs to ClientInfoUI.cs
    
    (sets up the scene for integrating swipe instead of click)

--- Assets/Scripts/CarScripts/CarFixing/Car.cs
+++ Assets/Scripts/CarScripts/CarFixing/Car.cs
@@ -51,8 +51,6 @@ public class Car : MonoBehaviour
     private bool startPosIsAssigned = false;
 
     public GameObject clientCard;
-    private List<Image> clientCardImages;
-    private float fadeSpeed = 1f;
 
   /// <summary>
   /// TODO: ADD method so that attackpoints spawn after the car has landed
@@ -73,14 +71,11 @@ public class Car : MonoBehaviour
 
         CreateClientCardVisual();
         clientCard.SetActive(false);
-        SetClientCardImages();
     }
 
     void Update()
     {
         ActivateClientCard();
-      
-        FadeOutClientCard();
 
         //check if first stage has been fixed
         if (attackPointsFixed >= firstStageAttackPointAmount && stagesDone == 0)
@@ -113,7 +108,6 @@ public class Car : MonoBehaviour
         }
     }
 
-
     private void ActivateClientCard()
     {
         if (hasLanded == true && startPosIsAssigned == false)
@@ -125,58 +119,14 @@ public class Car : MonoBehaviour
 
     }
 
-    private void FadeOutClientCard()
-    {
-        //fadout over time
-        if (clientCard.activeSelf == true)
-        {
-            StartCoroutine(Fade());
-        }
-    }
-
-
-
-
     private void CreateClientCardVisual()
     {
         //card creation and icon 
         var clientCard = Resources.Load<GameObject>("Clients/ClientCardPrefabs/ClientInfo");
         var x = Instantiate(clientCard, gm.canvas.transform.GetChild(3).GetChild(0).transform);
-        x.transform.GetChild(0).GetComponent<Image>().sprite = dynamicCarData.clientVisuals.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
-
-        //adding stars
-        for (int i = 0; i < dynamicCarData.starCount; i++)
-        {
-            var star = Resources.Load<GameObject>("Clients/ClientCardPrefabs/StarIcon");
-            Instantiate(star, x.transform.GetChild(1).transform);
-        }
-
-        //Adding order requirements
-        if (dynamicCarData.needWash == true)
-        {
-            x.transform.GetChild(2).transform.GetChild(1).gameObject.SetActive(true);
-        }
-        if (dynamicCarData.needFix == true)
-        {
-            x.transform.GetChild(2).transform.GetChild(0).gameObject.SetActive(true);
-        }
+        x.GetComponent<ClientInfoUI>().OnInstantiate(dynamicCarData);
 
         this.clientCard = x;
-    }
-
-
-
-    public IEnumerator Fade()
-    {
-        yield return new WaitForSeconds(2);
-
-        foreach (Image img in clientCardImages)
-        {
-            Color color = clientCard.GetComponent<Image>().color;
-            float fadeAmount = color.a - (fadeSpeed * Time.deltaTime);
-
-            img.color = new Color(color.r, color.g, color.b, fadeAmount);
-        }
     }
 
     #region Set Managers
@@ -277,16 +227,6 @@ public class Car : MonoBehaviour
             x.transform.GetChild(0).transform.localPosition = attackPointLocation.transform.GetChild(0).transform.localPosition;
             x.transform.GetChild(0).transform.rotation = attackPointLocation.transform.GetChild(0).transform.rotation;
         }
-    }
-
-    private void SetClientCardImages()
-    {
-        Image[] clientCardImages = clientCard.GetComponentsInChildren<Image>();
-        var clientCardImagesList = clientCardImages.ToList();
-
-        clientCardImagesList.Add(clientCard.GetComponent<Image>());
-
-        this.clientCardImages = clientCardImagesList;
     }
 
     public void FixAttackPoint()
--- Assets/Scripts/UIScripts/ClientInfoUI.cs
+++ Assets/Scripts/UIScripts/ClientInfoUI.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
 
 // Handles interaction with the Client Info Pop-up
-public class ClientInfoUI : MonoBehaviour
-{
-    //Managers
+public class ClientInfoUI : MonoBehaviour {
+    [SerializeField] private float fadeDelay = 2f;
+    [SerializeField] private float fadeSpeed = 1f;
+    private List<Image> clientCardImages;
+
+    private DynamicCarData dynamicCarData;
+
     private GameManager gm;
 
-    private void Awake() {
+    private void Start() {
         SetManager();
+        SetClientCardImages();
+    }
+
+    private void OnEnable() {
+        StartCoroutine(Fade());
+    }
+
+    // Sets the pop-up visuals based on the DynamicCarData
+    public void OnInstantiate(DynamicCarData dynamicCarData) {
+        this.dynamicCarData = dynamicCarData;
+
+        transform.GetChild(0).GetComponent<Image>().sprite = dynamicCarData.clientVisuals.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
+
+        // Adding stars
+        for (int i = 0; i < dynamicCarData.starCount; i++) {
+            var star = Resources.Load<GameObject>("Clients/ClientCardPrefabs/StarIcon");
+            Instantiate(star, transform.GetChild(1).transform);
+        }
+
+        // Adding order requirements
+        if (dynamicCarData.needWash == true)
+            transform.GetChild(2).transform.GetChild(1).gameObject.SetActive(true);
+        if (dynamicCarData.needFix == true)
+            transform.GetChild(2).transform.GetChild(0).gameObject.SetActive(true);
     }
 
     // TODO: Call this on swipe. For now it's called on click. 
@@ -20,4 +50,32 @@ public class ClientInfoUI : MonoBehaviour
     private void SetManager() {
         gm = FindObjectOfType<GameManager>();
     }
+
+    #region Display methods
+    private void SetClientCardImages() {
+        Image[] clientCardImages = GetComponentsInChildren<Image>();
+        var clientCardImagesList = clientCardImages.ToList();
+
+        clientCardImagesList.Add(GetComponent<Image>());
+
+        this.clientCardImages = clientCardImagesList;
+    }
+
+    public IEnumerator Fade() {
+        yield return new WaitForSeconds(fadeDelay);
+
+        float fadeAmount = 1f;
+        while (fadeAmount > 0.0f) {
+            foreach (Image img in clientCardImages) {
+                Color color = GetComponent<Image>().color;
+                fadeAmount = color.a - (fadeSpeed * Time.deltaTime);
+
+                img.color = new Color(color.r, color.g, color.b, fadeAmount);
+            }
+            yield return null;
+        }
+
+        gameObject.SetActive(false);
+    }
+    #endregion
 }
